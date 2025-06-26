cc492b0efe91a9e56a33b33f45e25dd5ecfc8489
JacobJohansson <jacobknuts@gmail.com>
2025-06-26 15:27:49 +0200

    ...

--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
+    return render_template("home.html")
+
+@views.route('/homer')
+def homer():
     return """
     <!DOCTYPE html>
     <html lang="en">
